8b79da38b5f07d7d32bfb0702cc5234cbe989178
marksymonm <marksymonmartinez05@gmail.com>
2025-09-16 00:58:36 +0800

    Update settings.py

--- OrderingSystem/OrderingSystem/settings.py
+++ OrderingSystem/OrderingSystem/settings.py
@@ -81,16 +81,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'OrderingSystem.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')  # Render automatically provides DATABASE_URL
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 
 # Password validation
